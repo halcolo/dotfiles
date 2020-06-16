43258d6a3599f253a6e756aea044ff8d0f860b2a
Carlos Sanchez JR <carlos1eduardo1sanchez1torres@gmail.com>
2020-06-15 20:42:01 -0700

    cest-ubuntu daily backup

--- shell/_functions/utils.sh
+++ shell/_functions/utils.sh
@@ -1,6 +1,7 @@
 function generate_backup() {
   crontab -l > mycron
   echo '0 20 * * *  /bin/zsh "$DOTFILES_PATH/scripts/package/export"' >> mycron
+  echo '0 18 * * *' gnome-session-quit --force
   crontab mycron
   rm mycron
 }
