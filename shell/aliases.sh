7111ccc39a7e4df769316bbd6e15e6f8a9031714
sanchezcarlosjr <hello@sanchezcarlosjr.com>
2021-02-19 19:54:18 -0800

    alias for python and go

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -27,3 +27,6 @@ alias i.='(idea $PWD &>/dev/null &)'
 alias c.='(code $PWD &>/dev/null &)'
 alias o.='open .'
 alias up='dot package update_all'
+
+# Languages
+alias python="python3"
--- shell/exports.sh
+++ shell/exports.sh
@@ -1,6 +1,6 @@
 export JAVA_HOME='/Library/Java/JavaVirtualMachines/amazon-corretto-15.jdk/Contents/Home'
 export GEM_HOME="$HOME/.gem"
-export GOPATH="$HOME/.go"
+export GOPATH="/usr/local/go/bin"
 
 export FZF_DEFAULT_OPTS='
   --color=pointer:#ebdbb2,bg+:#3c3836,fg:#ebdbb2,fg+:#fbf1c7,hl:#8ec07c,info:#928374,header:#fb4934
@@ -13,7 +13,7 @@ export path=(
   "$DOTFILES_PATH/bin"
   "$JAVA_HOME/bin"
   "$GEM_HOME/bin"
-  "$GOPATH/bin"
+  "$GOPATH"
   "$HOME/.cargo/bin"
   "/usr/local/opt/ruby/bin"
   "/usr/local/opt/python/libexec/bin"
